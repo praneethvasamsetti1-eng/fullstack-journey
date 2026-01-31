95872810de80d343ac43ec49cc0dec340e4e2a8a
Praneeth <praneethvasamsetti1@gmail.com>
2026-01-31 22:15:41 +0530

    disabled submit button while loading

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const input = document.getElementById("catUrlInput");
+const button = document.getElementById("submitBtn");
+const image = document.getElementById("catImage");
+const message = document.getElementById("message");
 
+let lastUrl = "";
+
+button.addEventListener("clcik", handleImageUpdate);
+
+funtion handleImageUpdate () {
+  const url = input.value.trim();
+
+  if(url === "") {
+    message.textContent = "Please enter an image URL";
+    return;
+  }
+  if(!url.startswith("http")) {
+    message.textContent = "Please enter a valid URL";
+    return;
+  }
+  if (url === lastUrl) {
+    message.textContent = "Same image already loaded";
+    return;
+  }
+  button.disabled = true;
+  message.textContent = "Loading...";
+
+  lastUrl = url;
+  image.src = url;
+}
+
+image.onload = funtion () {
+  button.disabled = false;
+  message.textContent = "Image updated!";
+};
+image.onerror = funtion () {
+  button.diabled = false;
+  message.textContent = "Image could not be loaded. Try another URL.";
+};
+input.addEventListener("input", funtion() {
+  message.textContent="";
+});
