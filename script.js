c50d44238a7702a48a7ad0bb9ac82e487bf389b1
Praneeth <praneethvasamsetti1@gmail.com>
2026-02-11 19:49:57 +0530

    added Async function

--- script.js
+++ script.js
@@ -45,3 +45,12 @@ image.onerror = function () {
 input.addEventListener("input", function () {
   message.textContent = "";
 });
+
+async function loadCat() {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const data = await res.json();
+  console.log(data);
+}
+
+loadCat();
+
